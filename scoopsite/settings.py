065d44b3adbaf18604cfabed79bb3e6e67fff877
fisayo_admin <fisayoadegun@gmail.com>
2020-08-29 10:41:14 +0100

    made some changes to settings.py

--- scoopsite/settings.py
+++ scoopsite/settings.py
@@ -150,4 +150,9 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 import django_heroku
 django_heroku.settings(locals())
 
+if os.environ.get('DEBUG') == 'TRUE':
+    DEBUG = True
+elif os.environ.get('DEBUG') == 'FALSE':
+    DEBUG = False
+
 
