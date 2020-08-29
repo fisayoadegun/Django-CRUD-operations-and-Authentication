2e3a6aa249d197c05629013e05215a6cafd8624e
fisayo_admin <fisayoadegun@gmail.com>
2020-08-29 08:38:08 +0100

    made some changes in settings.py

--- scoopsite/settings.py
+++ scoopsite/settings.py
@@ -88,10 +88,15 @@ WSGI_APPLICATION = 'scoopsite.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'fizzyflower',
+        'USER': 'fisayo_admin',
+        'PASSWORD': 'Surulere007',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
