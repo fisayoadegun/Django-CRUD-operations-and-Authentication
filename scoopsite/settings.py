be4d3d5b2224816907e1359b6a4cf0e234d5e0f0
fisayo_admin <fisayoadegun@gmail.com>
2020-08-29 14:50:28 +0100

    made some changes in logout.html, password

--- scoopsite/settings.py
+++ scoopsite/settings.py
@@ -154,5 +154,9 @@ if os.environ.get('DEBUG') == 'TRUE':
     DEBUG = True
 elif os.environ.get('DEBUG') == 'FALSE':
     DEBUG = False
+# Heroku: Update database configuration from $DATABASE_URL.
+import dj_database_url
 
+db_from_env = dj_database_url.config(conn_max_age=500)
+DATABASES['default'].update(db_from_env)
 
