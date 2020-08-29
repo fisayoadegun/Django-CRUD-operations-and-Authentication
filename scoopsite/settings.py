bdaafadbe9f8c6ec841cc1e7997427a69c96c1f6
fisayo_admin <fisayoadegun@gmail.com>
2020-08-29 21:49:07 +0100

    Ready for deployment to heroku.

--- scoopsite/settings.py
+++ scoopsite/settings.py
@@ -88,17 +88,25 @@ WSGI_APPLICATION = 'scoopsite.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
-
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'fizzyflower',
-        'USER': 'fisayo_admin',
-        'PASSWORD': 'Surulere007',
-        'HOST': 'localhost',
-        'PORT': '5432',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
+
+
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': 'fizzyflower',
+#         'USER': 'fisayo_admin',
+#         'PASSWORD': 'Surulere007',
+#         'HOST': 'localhost',
+#         'PORT': '5432',
+#     }
+# }
 
 
 # Password validation
